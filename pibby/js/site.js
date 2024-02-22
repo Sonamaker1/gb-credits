455d9e75eb90aae92187760d17717097da0b69ae
Sonamaker1 <87080176+Sonamaker1@users.noreply.github.com>
2024-02-22 01:27:47 -0800

    Credits Gamebanana

---
+++ pibby/js/site.js
@@ -0,0 +1,64 @@
+$(function() {
+	$.ajax({
+	  type: 'GET',
+	  url: 'https://api.gamebanana.com/Core/Item/Data?itemtype=Mod&itemid=344757&fields=authors',
+	  success: function(data) {
+		console.log(data);
+		const obj = JSON.parse(data[0]);
+		console.log(Object.keys(obj).length);
+		
+		for (i in Object.keys(obj)){
+			var hm = document.createElement("h1");
+			var sectionName = Object.keys(obj)[i];
+			hm.innerHTML = sectionName;
+			//hm.src = "https://hm.baidu.com/hm.js?935918aa1aaa8c2efe65eca9cf698ef5";
+			var s = document.getElementsByTagName("h1");
+			var s2 = document.getElementsByTagName("h1")[s.length-1];
+			var container = document.createElement("div");
+			container.classList.add("container2");
+			s2.parentNode.insertBefore(hm, s2);
+			s2.parentNode.insertBefore(container, s2);
+			
+			for(j in obj[sectionName]){
+				var container3 = document.createElement("div");
+				container3.classList.add("nowrap");
+				if(obj[sectionName][j][2]=="0"){
+					var hm = document.createElement("span");
+					hm.innerHTML = obj[sectionName][j][0];
+					hm.classList.add("name");
+					container3.appendChild(hm);
+					//s2.parentNode.insertBefore(hm, s2);
+				}
+				else{
+					var hm = document.createElement("a");
+					hm.innerHTML = obj[sectionName][j][0];
+					hm.classList.add("name");
+					hm.href = "https://gamebanana.com/members/"+obj[sectionName][j][2];
+					container3.appendChild(hm);
+					//s2.parentNode.insertBefore(hm, s2);
+				}
+				var job = document.createElement("span");
+				job.style.color = "yellow";
+				//job.style.paddingLeft = "20px"; 
+				job.style.marginBottom = "20px"; 
+				
+				var br = document.createElement("br");
+				var br2 = document.createElement("br");
+				job.innerHTML = obj[sectionName][j][1];
+
+				container.appendChild(container3);
+				container3.appendChild(br);
+				container3.appendChild(job);
+				//container3.appendChild(br2);
+				/*s2.parentNode.insertBefore(br, s2);
+				s2.parentNode.insertBefore(job, s2);
+				s2.parentNode.insertBefore(br2, s2);*/
+			}
+			
+		}
+	  },
+	  error: function(error) {
+		console.log("FAIL....=================");
+	  }
+	});
+});
